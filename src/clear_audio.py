11b386b425ec324904ba441a6efb4914274e36e3
Алиса Хайдарова <i.nesy.chav@gmail.com>
2024-10-12 11:39:22 +0300

    Добавлены комментарии

--- src/clear_audio.py
+++ src/clear_audio.py
@@ -1,43 +1,26 @@
 import librosa
 import noisereduce as nr
-import matplotlib.pyplot as plt
 import soundfile as sf
 
-import os
 from decorates import time_memory
-from typing import Optional, Any
+from typing import Optional, Any, Tuple
 
 
 @time_memory
-def clean_audio(audio_file: str) -> Optional[tuple[Any, int | float]]:
-    if not os.path.exists(audio_file):
-        print('Файл не найден')
-        return None
+def clean_audio(audio_file: str) -> Optional[Tuple[Any, int | float]]:
+    """
+    Функция очистки аудио от шумов
+
+    :param audio_file: Путь к аудиофайлу
+    :return: Массив значений амплитуды аудиосигнала и частота дискретизации
+    """
 
     # Загрузка MP3 файла
-    y, sr = librosa.load(audio_file, sr=None)
+    y, sr = librosa.load(audio_file, sr=16000)
 
     # Подавление шума
     y_denoised = nr.reduce_noise(y=y, sr=sr)
 
-    # Визуализация
-    # plt.figure(figsize=(12, 8))
-    #
-    # # Оригинальный сигнал
-    # plt.subplot(2, 1, 1)
-    # librosa.display.waveshow(y, sr=sr, alpha=0.5)
-    # plt.title('Оригинальный сигнал')
-    # plt.xlabel('Время (с)')
-    # plt.ylabel('Амплитуда')
-    #
-    # # Очищенный сигнал
-    # plt.subplot(2, 1, 2)
-    # librosa.display.waveshow(y_denoised, sr=sr, color='orange', alpha=0.5)
-    # plt.title('Очищенный сигнал')
-    # plt.xlabel('Время (с)')
-    # plt.ylabel('Амплитуда')
-    #
-    # plt.tight_layout()
-    # plt.show()
+    # sf.write('output2.wav', y_denoised, sr)
 
     return y_denoised, sr
--- src/command_from_text.py
+++ src/command_from_text.py
@@ -1,34 +1,43 @@
 import re
 import spacy
 
-from typing import Optional
+from typing import Union, Tuple
 from decorates import time_memory
 
 _nlp = spacy.load("ru_core_news_sm")
 
 
 @time_memory
-def find_command_in_text(text: str, commands: dict[str: int]) -> Optional[str | tuple[str, int]]:
+def find_command_in_text(text: str, commands: list) -> Union[Tuple[int, int] | Tuple[str, int]]:
+    """
+    Функция нахождения команды в тексте
+
+    :param text: Исходный текст
+    :param commands: Набор команд
+    :return: Команду и её параметр, иначе -1
+    """
 
     text_lower = text.lower()
 
+    # Лемматизация слов в тексте
     doc = _nlp(text_lower)
     words_text = ' '.join([token.lemma_ for token in doc if not token.is_stop])
 
-    for command, _ in commands.items():
+    for command in commands:
+
+        # Лемматизация слов в команде
         doc = _nlp(command)
         words_command = ' '.join([token.lemma_ for token in doc if not token.is_stop])
 
+        # Проверка наличия команды в тексте
         if re.search(r'\( количество \)', words_command):
             pattern = words_command.replace("( количество )", r"(\d+)")
             find_command = re.search(pattern, words_text)[0]
             if find_command:
                 number = re.search(r'(\d+)', find_command)[0]
                 return command, int(number)
-        else:
-            pattern = words_command
 
-        if re.search(pattern, words_text):
-            return command
+        elif re.search(words_command, words_text):
+            return command, -1
 
-    return None
+    return -1, -1
